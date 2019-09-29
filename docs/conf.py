1bf70e404fa4b1b2196d4e4dd6e2551bd078b82e
John Murray <me@johnmurray.io>
2019-09-29 17:24:06 -0400

    RtD fixes

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,30 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+	
+import subprocess, os
+ 
+def configureDoxyfile(input_dir, output_dir):
+    with open('Doxyfile.in', 'r') as file :
+        filedata = file.read()
+ 
+    filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
+    filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
+ 
+    with open('Doxyfile', 'w') as file:
+        file.write(filedata)
+ 
+# Check if we're running on Read the Docs' servers
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+ 
+breathe_projects = {}
+ 
+if read_the_docs_build:
+    input_dir = '../pembroke'
+    output_dir = 'build'
+    configureDoxyfile(input_dir, output_dir)
+    subprocess.call('doxygen', shell=True)
+    breathe_projects['Pembroke'] = output_dir + '/xml'
 
 
 # -- Project information -----------------------------------------------------
